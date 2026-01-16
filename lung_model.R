7a73b496eec27b8e4938d3c2f9f2c5fc1d6edf53
Lydia Hefel <lydhefel@gmail.com>
2026-01-16 12:08:29 -1000

    lung model practice

---
+++ lung_model.R
@@ -0,0 +1,14 @@
+install.packages("tidyverse")
+install.packages("lme4")
+
+library(tidyverse)
+library(lme4)
+
+lung <- read_csv("data/lung.csv")
+
+ggplot(lung, aes(Exposure, PEFR)) +
+  geom_point()
+
+model <- lm(PEFR ~ Exposure, data = lung)
+summary(model)
+
